6524d94919c442e5adcba54fe9b89f69c4ef3dae
swinolani <boulaabi.mohamed1997@gmail.com>
2023-11-27 21:05:07 +0100

    amelioration code pour la liste mots

--- Training-exo/Bac-A-Sable/Bac-A-Sable.js
+++ Training-exo/Bac-A-Sable/Bac-A-Sable.js
@@ -1,65 +1,50 @@
 // CALCUL MENTAL !
 
 // //Méthode utiles
-// function nombre_1_a_100() {
-//   return Math.floor(Math.random() * 100);
-// }
+function nombre_1_a_100() {
+  return Math.floor(Math.random() * 100);
+}
 
-// //Variable
-// const calcul = document.getElementById("calcul");
-// const valider = document.getElementById("valider");
-// let result1 = nombre_1_a_100();
-// let result2 = nombre_1_a_100();
+//Variable
+const calcul = document.getElementById("calcul");
+const valider = document.getElementById("valider");
+let result1 = nombre_1_a_100();
+let result2 = nombre_1_a_100();
 
-// //Placement de l'input
-// calcul.innerHTML = `${result1 < 10 ? `0${result1}` : result1} × ${
-//   result2 < 10 ? `0${result2}` : result2
-// } = <input type="text" id="result" placeholder="????" maxlength="4" />`;
+//Placement de l'input
+calcul.innerHTML = `${result1 < 10 ? `0${result1}` : result1} × ${
+  result2 < 10 ? `0${result2}` : result2
+} = <input type="text" id="result" placeholder="????" maxlength="4" />`;
 
-// // Début des festivités
-// document.addEventListener("keyup", function (e) {
-//   if (e.key == "Enter") {
-//     const result = document.getElementById("result");
-//     if (valider.textContent == "Valider") {
-//       if (parseInt(result.value) == result1 * result2) {
-//         result.value = "";
-//         calcul.innerHTML = `${result1 < 10 ? `0${result1}` : result1} × ${
-//           result2 < 10 ? `0${result2}` : result2
-//         } = ${result1 * result2}`;
-//         calcul.style.color = "green";
-//         valider.textContent = "Suivant";
-//       } else {
-//         calcul.innerHTML = `${result1 < 10 ? `0${result1}` : result1} × ${
-//           result2 < 10 ? `0${result2}` : result2
-//         } = ${result1 * result2}`;
-//         calcul.style.color = "red";
-//         result.value = "";
-//         valider.textContent = "Suivant";
-//       }
-//     } else if (valider.textContent == "Suivant") {
-//       result1 = nombre_1_a_100();
-//       result2 = nombre_1_a_100();
-//       calcul.innerHTML = `${result1 < 10 ? `0${result1}` : result1} × ${
-//         result2 < 10 ? `0${result2}` : result2
-//       } = <input type="text" id="result" placeholder="????" maxlength="4" />`;
-//       calcul.style.color = "";
-//       valider.textContent = "Valider";
-//     }
-//   }
-//   result.focus();
-// });
-// -----------------------------------------------
-
-let listeMots = [];
-async function listeMotsbis() {
-  const response = await fetch("./assets/csvjson.json");
-  const liste = await response.json();
-  for (let i = 0; i < liste.length; i++) {
-    listeMots.push(liste[i].a);
+// Début des festivités
+document.addEventListener("keyup", function (e) {
+  if (e.key == "Enter") {
+    const result = document.getElementById("result");
+    if (valider.textContent == "Valider") {
+      if (parseInt(result.value) == result1 * result2) {
+        result.value = "";
+        calcul.innerHTML = `${result1 < 10 ? `0${result1}` : result1} × ${
+          result2 < 10 ? `0${result2}` : result2
+        } = ${result1 * result2}`;
+        calcul.style.color = "green";
+        valider.textContent = "Suivant";
+      } else {
+        calcul.innerHTML = `${result1 < 10 ? `0${result1}` : result1} × ${
+          result2 < 10 ? `0${result2}` : result2
+        } = ${result1 * result2}`;
+        calcul.style.color = "red";
+        result.value = "";
+        valider.textContent = "Suivant";
+      }
+    } else if (valider.textContent == "Suivant") {
+      result1 = nombre_1_a_100();
+      result2 = nombre_1_a_100();
+      calcul.innerHTML = `${result1 < 10 ? `0${result1}` : result1} × ${
+        result2 < 10 ? `0${result2}` : result2
+      } = <input type="text" id="result" placeholder="????" maxlength="4" />`;
+      calcul.style.color = "";
+      valider.textContent = "Valider";
+    }
   }
-}
-async function mot_le_plus_long() {
-  await listeMotsbis();
-  console.log(listeMots);
-}
-mot_le_plus_long();
+  result.focus();
+});
