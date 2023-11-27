4b4aa37be136a194f4b51ec920c31e6c47a3b9ee
Swinolani <boulaabi.mohamed1997@gmail.com>
2023-11-27 09:57:10 +0100

    modif projet jeu math

--- Training-exo/Bac-A-Sable/Bac-A-Sable.js
+++ Training-exo/Bac-A-Sable/Bac-A-Sable.js
@@ -10,37 +10,39 @@ let result1 = nombre_1_a_100();
 let result2 = nombre_1_a_100();
 
 //Placement de l'input
-calcul.innerHTML = `${result1 < 10 ? `0${result1}` : result1} + ${
+calcul.innerHTML = `${result1 < 10 ? `0${result1}` : result1} × ${
   result2 < 10 ? `0${result2}` : result2
-} = <input type="text" id="result" placeholder="??" maxlength="3" />`;
+} = <input type="text" id="result" placeholder="????" maxlength="4" />`;
 
 // Début des festivités
-const result = document.getElementById("result");
-valider.addEventListener("click", function () {
-  if (valider.textContent == "Valider") {
-    if (parseInt(result.value) == result1 + result2) {
-      result.value = "";
-      calcul.innerHTML = `${result1 < 10 ? `0${result1}` : result1} + ${
+document.addEventListener("keyup", function (e) {
+  if (e.key == "Enter") {
+    const result = document.getElementById("result");
+    if (valider.textContent == "Valider") {
+      if (parseInt(result.value) == result1 * result2) {
+        result.value = "";
+        calcul.innerHTML = `${result1 < 10 ? `0${result1}` : result1} × ${
+          result2 < 10 ? `0${result2}` : result2
+        } = ${result1 * result2}`;
+        calcul.style.color = "green";
+        valider.textContent = "Suivant";
+      } else {
+        calcul.innerHTML = `${result1 < 10 ? `0${result1}` : result1} × ${
+          result2 < 10 ? `0${result2}` : result2
+        } = ${result1 * result2}`;
+        calcul.style.color = "red";
+        result.value = "";
+        valider.textContent = "Suivant";
+      }
+    } else if (valider.textContent == "Suivant") {
+      result1 = nombre_1_a_100();
+      result2 = nombre_1_a_100();
+      calcul.innerHTML = `${result1 < 10 ? `0${result1}` : result1} × ${
         result2 < 10 ? `0${result2}` : result2
-      } = ${result1 + result2}`;
-      calcul.style.color = "green";
-      valider.textContent = "Suivant";
-    } else {
-      calcul.innerHTML = `${result1 < 10 ? `0${result1}` : result1} + ${
-        result2 < 10 ? `0${result2}` : result2
-      } = ${result1 + result2}`;
-      calcul.style.color = "red";
-      result.value = "";
-      valider.textContent = "Suivant";
+      } = <input type="text" id="result" placeholder="????" maxlength="4" />`;
+      calcul.style.color = "";
+      valider.textContent = "Valider";
     }
-  } else if (valider.textContent == "Suivant") {
-    result1 = nombre_1_a_100();
-    result2 = nombre_1_a_100();
-    calcul.innerHTML = `${result1 < 10 ? `0${result1}` : result1} + ${
-      result2 < 10 ? `0${result2}` : result2
-    } = <input type="text" id="result" placeholder="??" maxlength="3" />`;
-    calcul.style.color = "";
-    valider.textContent = "Valider";
   }
-  console.log(result1, result2);
+  result.focus();
 });
