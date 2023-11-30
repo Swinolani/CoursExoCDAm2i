407a7fdcf070dd0139252067c8bbb4d3e02b9085
swinolani <boulaabi.mohamed1997@gmail.com>
2023-11-30 23:52:29 +0100

    avancement projet chiffre

--- Training-exo/Bac-A-Sable/desChiffres.js
+++ Training-exo/Bac-A-Sable/desChiffres.js
@@ -1,5 +1,13 @@
 // Fonctions utiles-----------------------------------------------
-
+function contenanceBonChiffre(chaine) {
+  let listeChiffreCorrecte = new RegExp(/\b(?:[1-9]|10|25|50|75|100)\b/);
+  for (let c of chaine.split(" ")) {
+    if (c.match(listeChiffreCorrecte) == null) {
+      return false;
+    }
+  }
+  return true;
+}
 // comparaison de 2 tableau
 function egaliteEntreListes(liste1, liste2) {
   if (liste1.length != liste2.length) {
@@ -23,7 +31,7 @@ function listeEstDansGrandeListe(liste, grosseListe) {
   return false;
 }
 // Tableau de tout les arrangements d'un tableau de nombre
-const listePermutations = function (nums) {
+function listePermutations(nums) {
   let result = [];
   const backtrack = (i, nums) => {
     if (i === nums.length) {
@@ -45,7 +53,7 @@ const listePermutations = function (nums) {
   }
 
   return resultTronque;
-};
+}
 
 // Liste toutes les combinaisons opératoire
 function generateOperatorCombinations(
@@ -73,20 +81,30 @@ function generateOperatorCombinations(
   return result;
 }
 
-// Utilisation avec votre liste d'opérateurs et une longueur de 5
-const operators = ["*", "/", "+", "-"];
-const result = generateOperatorCombinations(operators);
+console.log(1024 * 720);
 
 // -------------------------------------------------------
 
 // Corps du code pour les chiffres
-
-function bonCompte(sequenceChiffre, nombre) {
-  let listChiffre = sequenceChiffre.split(" ");
-
-  let listPermutationsChiffre = listePermutations(listChiffre);
-  leCompteEstPasBon = true;
-  while (leCompteEstPasBon) {}
+let listeLettre = new RegExp("[a-zA-Z]");
+let listChiffre = prompt(
+  "Entrez vos 6 nombres espacé contenant au coix ces chiffres : 1,2,3,4,5,6,7,8,9,10,25,50,75,100"
+);
+while (
+  listChiffre.match(listeLettre) != null ||
+  listChiffre.split(" ").length - 1 != 5 ||
+  !contenanceBonChiffre(listChiffre) ||
+  listChiffre.split(" ").length != 6
+) {
+  listChiffre = prompt(
+    "La saisie est incorrecte, vous devez entrer 6 nombres espacés parmi ces chiffres : 1,2,3,4,5,6,7,8,9,10,25,50,75,100\nExemple : '2 25 7 10 100 8'"
+  );
 }
 
+// Faire la fusion des 2 élément sans parenthésage
+// Utilisation des élément necessaire résultant de tout le code au dessus !
+
+const listeOperateur = generateOperatorCombinations(["*", "/", "+", "-"]);
+const listeArrangementsNombres = listePermutations(listChiffre.split(" "));
+// SUITE ICI
 // --------------------------------------
